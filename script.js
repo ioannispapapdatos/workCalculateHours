57b1bec4dd7300780bd720e709897a6fd5ca56de
Jannis Paps <jannispaps@Janniss-MBP.home>
2022-01-31 23:43:05 +0200

    new

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 // we select our values
 // total biz hours
-
-let biz_hours = document.querySelector("#biz_hours").value;
-let monthBonus = document.querySelector("#month_bonus").value;
-let totalStaff = document.querySelector("#total_staff").value;
-let staffHours = document.querySelector("#staff_hours").value;
-
-let hourValue = document.querySelector("#hour_value").value;
-
+// we select the vslues
+let openBizHours = document.querySelector("#biz_hours").value;
+let monthBonusMoney = document.querySelector("#month_bonus").value;
+let workStaff = document.querySelector("#total_staff").value;
+// let staffHours = document.querySelector("#staff_hours").value;
+// let hourValue = document.querySelector("#hour_value").value;
 let totalBonus = document.querySelector("#display");
 // button selection
 const calculate = document.querySelector("#calculate");
-const reset = document.querySelector("#creset");
+const reset = document.querySelector("#reset");
 
-// hour value= biz hours/ month bonus
+// display selection
+const workHourDisplay = document.querySelector("#work_hour_display");
 
 calculate.addEventListener("click", function () {
-  let biz_hours = document.querySelector("#biz_hours").value;
-  let monthBonus = document.querySelector("#month_bonus").value;
-  let totalStaff = document.querySelector("#total_staff").value;
-  let staffHours = document.querySelector("#staff_hours").value;
+  let openBizHours = document.querySelector("#biz_hours").value;
+  let monthBonusMoney = document.querySelector("#month_bonus").value;
+  let workStaff = document.querySelector("#total_staff").value;
+  calculation = monthBonusMoney / openBizHours / workStaff;
+  workHourDisplay.innerHTML = calculation;
 
-  let hourValue = document.querySelector("#hour_value").value;
-  hourValue = monthBonus / biz_hours / totalStaff;
   //   totalBonus=0
-
-  totalBonus.innerHTML = hourValue;
 });
+
+// reset btn fun
+
+// function reset() {
+//   const resetBtn = document.querySelector("#reset");
+//   resetBtn = "file:///Users/jannispaps/Desktop/workHour_Calc/index.html";
+//   resetBtn.innerHTML = resetBtn;
+// }
