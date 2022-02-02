eecd5ffe898cf209e38f288aa85d4cf018fafa69
Jannis Paps <jannispaps@Janniss-MBP.home>
2022-02-02 08:54:45 +0200

    new

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
 // we select our values
 // total biz hours
 // we select the vslues
-let openBizHours = document.querySelector("#biz_hours").value;
-let monthBonusMoney = document.querySelector("#month_bonus").value;
-let workStaff = document.querySelector("#total_staff").value;
-// let staffHours = document.querySelector("#staff_hours").value;
-// let hourValue = document.querySelector("#hour_value").value;
-let totalBonus = document.querySelector("#display");
+const openBizHours = document.querySelector("#biz_hours").value;
+const monthBonusMoney = document.querySelector("#month_bonus").value;
+const workStaff = document.querySelector("#total_staff").value;
+const totalBonus = document.querySelector("#display");
+
 // button selection
 const calculate = document.querySelector("#calculate");
-const reset = document.querySelector("#reset");
 
 // display selection
 const workHourDisplay = document.querySelector("#work_hour_display");
+const displayDiv = document.querySelector("#displayDiv");
+
+// fuctions start here
 
 calculate.addEventListener("click", function () {
   let openBizHours = document.querySelector("#biz_hours").value;
   let monthBonusMoney = document.querySelector("#month_bonus").value;
   let workStaff = document.querySelector("#total_staff").value;
-  calculation = monthBonusMoney / openBizHours / workStaff;
+  let calculation = monthBonusMoney / openBizHours / workStaff;
+  workHourDisplay.innerHTML = calculation;
 
-  if (calculation) {
-    workHourDisplay.innerHTML = calculation;
-  }
-  if (monthBonusMoney || workStaff) {
-    workHourDisplay.style.color = "red";
-    workHourDisplay.innerHTML = "Please Put all Values !!!";
-  } else {
+  if (!openBizHours || !monthBonusMoney || !workStaff) {
     workHourDisplay.style.color = "red";
     workHourDisplay.innerHTML = "Please Put all Values !!!";
   }
-
-  //   totalBonus=0
 });
 
 // reset btn fun
 
-// function reset() {
-//   const resetBtn = document.querySelector("#reset");
-//   resetBtn = "file:///Users/jannispaps/Desktop/workHour_Calc/index.html";
-//   resetBtn.innerHTML = resetBtn;
-// }
+const resetBtn = document.querySelector("#resetBtn");
+resetBtn.addEventListener("click", function () {
+  workHourDisplay.innerHTML = 0;
+  monthBonusMoney.innerHTML = 0;
+});
+
+// toolip massege code
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+// p value msg function
