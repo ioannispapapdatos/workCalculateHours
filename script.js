18e41b5c91b29d8287c4cf665fbb7adbd8f094e0
Jannis Paps <jannispaps@Janniss-MBP.home>
2022-02-15 09:45:14 +0200

    new uplod

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // we select our values
 // total biz hours
-// we select the vslues
+// we select the values
 const openBizHours = document.querySelector("#biz_hours").value;
 const monthBonusMoney = document.querySelector("#month_bonus").value;
 const workStaff = document.querySelector("#total_staff").value;
 const totalBonus = document.querySelector("#display");
+const staff_value_display = document.querySelector("#staff_value_display");
 
 // button selection
 const calculate = document.querySelector("#calculate");
@@ -16,15 +17,20 @@ const displayDiv = document.querySelector("#displayDiv");
 // fuctions start here
 
 calculate.addEventListener("click", function () {
-  let openBizHours = document.querySelector("#biz_hours").value;
+  const openBizHours = document.querySelector("#biz_hours").value;
   let monthBonusMoney = document.querySelector("#month_bonus").value;
   let workStaff = document.querySelector("#total_staff").value;
   let calculation = monthBonusMoney / openBizHours / workStaff;
-  workHourDisplay.innerHTML = calculation;
+  const staffValueCalc = calculation / workStaff;
+  workHourDisplay.innerHTML = `${calculation} euro`;
+  staff_value_display.innerHTML = ` ${staffValueCalc} euro`;
 
   if (!openBizHours || !monthBonusMoney || !workStaff) {
-    workHourDisplay.style.color = "red";
+    // workHourDisplay.style.backgroundColor = "red";
+    // staff_value_display.style.backgroundColor = "red";
     workHourDisplay.innerHTML = "Please Put all Values !!!";
+    staff_value_display.innerHTML = "Missing Values";
+    // staff_value_display.style.color = "red";
   }
 });
 
@@ -32,11 +38,13 @@ calculate.addEventListener("click", function () {
 
 const resetBtn = document.querySelector("#resetBtn");
 resetBtn.addEventListener("click", function () {
+  // preventDefault();
+
   workHourDisplay.innerHTML = 0;
-  monthBonusMoney.innerHTML = 0;
+  staff_value_display.innerHTML = 0;
 });
 
-// toolip massege code
+// toolip message code
 var tooltipTriggerList = [].slice.call(
   document.querySelectorAll('[data-bs-toggle="tooltip"]')
 );
